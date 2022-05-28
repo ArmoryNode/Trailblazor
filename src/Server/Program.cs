beb5e59fb959aaad2391b2c168270aa84c527d48
ArmoryNode <andy@shapemydistrict.com>
2022-05-28 16:44:28 -0700

    Updated microsoft account graph client to handle 404s

--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Graph.Models.ODataErrors;
 using System.Net.Http.Headers;
 using System.Security.Claims;
 using Trailblazor.Server.Data;
@@ -55,17 +56,26 @@ builder.Services.AddAuthentication()
         {
             if (context.Identity is ClaimsIdentity identity)
             {
-                // Create an http client and pass the user's access token.
-                // We'll need this to make a request to the Microsoft Graph on behalf of the user.
-                using var httpClient = new HttpClient();
-                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(OidcConstants.AuthenticationSchemes.AuthorizationHeaderBearer, context.AccessToken);
+                try
+                {
+                    // Create an http client and pass the user's access token.
+                    // We'll need this to make a request to the Microsoft Graph on behalf of the user.
+                    using var httpClient = new HttpClient();
+                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(OidcConstants.AuthenticationSchemes.AuthorizationHeaderBearer, context.AccessToken);
 
-                // Create a new Graph client to make getting resources from the Microsoft Graph easier.
-                var graphClient = new Microsoft.Graph.GraphServiceClient(httpClient);
+                    // Create a new Graph client to make getting resources from the Microsoft Graph easier.
+                    var graphClient = new Microsoft.Graph.GraphServiceClient(httpClient);
 
-                // Get a stream of the user's profile image.
-                using var photoStream = await graphClient.Me.Photos["120x120"].Content.GetAsync();
-                context.Principal.AddBase64ImageClaim(photoStream);
+                    // Get a stream of the user's profile image.
+                    using var photoStream = await graphClient.Me.Photos["120x120"].Content.GetAsync(cancellationToken: context.HttpContext.RequestAborted);
+                    context.Principal.AddBase64ImageClaim(photoStream);
+                }
+                catch (ODataError)
+                {
+                    // If there is an error getting the profile image from the Microsoft Graph, then use the placeholder.
+                    using var photoStream = File.OpenRead(Path.Combine(builder.Environment.WebRootPath, "img/placeholder-profile-image.svg"));
+                    context.Principal.AddBase64ImageClaim(photoStream);
+                }
             }
         };
 
