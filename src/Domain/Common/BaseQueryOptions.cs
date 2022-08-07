2218afe01ad968449065287d6bd290d2569834d0
ArmoryNode <andy@shapemydistrict.com>
2022-08-06 21:24:38 -0700

    Added data service interface and base query options type

---
+++ src/Domain/Common/BaseQueryOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trailblazor.Domain.Common
+{
+    public record BaseQueryOptions
+    {
+        public Guid? Id { get; init; }
+        public string? SearchText { get; init; }
+    }
+}
---
+++ src/Server/Services/IDataService.cs
@@ -0,0 +1,12 @@
+﻿using Trailblazor.Domain.Common;
+
+namespace Trailblazor.Server.Services
+{
+    public interface IDataService<TViewModel>
+    {
+        Task<TViewModel> GetById(Guid entityId, CancellationToken cancellationToken);
+        Task<TViewModel[]> GetAll(BaseQueryOptions queryOptions, CancellationToken cancellationToken);
+        Task Delete(Guid entityId, CancellationToken cancellationToken);
+        Task Save(TViewModel viewModel, CancellationToken cancellationToken);
+    }
+}
